85b22894eed2d27334d30dc864218cd55982ba1d
Clemens Walbrodt <d0nk@rautemusik.fm>
2014-10-13 21:25:04 +0200

    optimize mysql utf8 connection 	modified:   includes/dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -7,5 +7,4 @@ require(dirname(__FILE__)."/../config/dbconfig.inc.php");
 
 $dbh = mysql_connect($dbhostname,$dbusername,$dbpassword) or die("Could not connect to database server, please check servername and credentials.");
 $dbs = mysql_select_db($dbname, $dbh) or die("There was a problem selecting the database, please check database name.");
-
-?>
+mysql_query("set names 'utf8';", $dbh);
