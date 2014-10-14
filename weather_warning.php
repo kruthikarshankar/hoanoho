bc96966533637d98e4bdc6fafc26528bffb2dc47
Julian Pawlowski <julian.pawlowski@gmail.com>
2014-10-14 13:57:22 +0200

    check for dwd_state value

--- weather_warning.php
+++ weather_warning.php
@@ -24,8 +24,15 @@
 
     <section class="main_weather">
         <h1><span>Wetter (Unwetter-) Warnungen</span></h1>
-            <div id="radar"><a href="http://www.dwd.de/bvbw/appmanager/bvbw/dwdwwwDesktop?_nfpb=true&_windowLabel=T14600649251144330032285&_urlType=action&_pageLabel=_dwdwww_wetter_warnungen_warnungen&WEEKLY_REPORT_VIEW=false&TIME=x&SHOW_HEIGHT_SEL=true&MAP_VIEW=true&MOVIE_VIEW=false&TABLE_VIEW=false&HEIGHT=x&SHOW_TIME_SEL=true&STATIC_CONTENT_VIEW=false&WARNING_TYPE=0&_state=maximized&LAND_CODE=<?= $__CONFIG['dwd_state'] ?>" target="_blank"><img src="http://www.dwd.de/dyn/app/ws/maps/<?= $__CONFIG['dwd_state'] ?>_x_x_0.gif"></a></div>
             <?php
+            if ($__CONFIG['dwd_state'] != "") {
+            ?>
+
+            <div id="radar"><a href="http://www.dwd.de/bvbw/appmanager/bvbw/dwdwwwDesktop?_nfpb=true&_windowLabel=T14600649251144330032285&_urlType=action&_pageLabel=_dwdwww_wetter_warnungen_warnungen&WEEKLY_REPORT_VIEW=false&TIME=x&SHOW_HEIGHT_SEL=true&MAP_VIEW=true&MOVIE_VIEW=false&TABLE_VIEW=false&HEIGHT=x&SHOW_TIME_SEL=true&STATIC_CONTENT_VIEW=false&WARNING_TYPE=0&_state=maximized&LAND_CODE=<?= $__CONFIG['dwd_state'] ?>" target="_blank"><img src="http://www.dwd.de/dyn/app/ws/maps/<?= $__CONFIG['dwd_state'] ?>_x_x_0.gif"></a></div>
+
+            <?php
+            }
+
             if (isset($dwd_warnung) && $dwd_warnung != "") {
             ?>
             <div id="title">Warnung</div>
