beaa0d04eb6cc06c9c993d0de72438917cc136f2
Julian Pawlowski <julian.pawlowski@gmail.com>
2014-10-14 16:50:55 +0200

    add new favicons closes #18

--- configuration_users.php
+++ configuration_users.php
@@ -36,8 +36,27 @@
 
         <?php include dirname(__FILE__).'/includes/getUserSettings.php'; ?>
 
-        <link rel="apple-touch-icon" href="./img/favicon.ico"/>
-        <link rel="shortcut icon" type="image/x-icon" href="./img/favicon.ico" />
+        <link rel="shortcut icon" href="./img/favicons/favicon.ico">
+        <link rel="apple-touch-icon" sizes="57x57" href="./img/favicons/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="./img/favicons/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="./img/favicons/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="./img/favicons/apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="./img/favicons/apple-touch-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="./img/favicons/apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="./img/favicons/apple-touch-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="./img/favicons/apple-touch-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="./img/favicons/apple-touch-icon-180x180.png">
+        <meta name="apple-mobile-web-app-title" content="Hoanoho">
+        <link rel="icon" type="image/png" href="./img/favicons/favicon-192x192.png" sizes="192x192">
+        <link rel="icon" type="image/png" href="./img/favicons/favicon-160x160.png" sizes="160x160">
+        <link rel="icon" type="image/png" href="./img/favicons/favicon-96x96.png" sizes="96x96">
+        <link rel="icon" type="image/png" href="./img/favicons/favicon-16x16.png" sizes="16x16">
+        <link rel="icon" type="image/png" href="./img/favicons/favicon-32x32.png" sizes="32x32">
+        <meta name="msapplication-TileColor" content="#603cba">
+        <meta name="msapplication-TileImage" content="./img/favicons/mstile-144x144.png">
+        <meta name="msapplication-config" content="./img/favicons/browserconfig.xml">
+        <meta name="application-name" content="Hoanoho">
+
         <title><?php echo $__CONFIG['main_sitetitle'] ?> - Einstellungen - Benutzer</title>
 
         <script type="text/javascript" src="./js/jquery.min.js"></script>
