66b75f2fe10a38d6201584b466242a1695f70331
Julian Pawlowski <julian.pawlowski@gmail.com>
2014-10-15 02:13:37 +0200

    exclude subregions

--- configuration_settings.php
+++ configuration_settings.php
@@ -44,7 +44,7 @@
           case"dwd_region":
     				echo "<select name=\"".$object->configstring."\" style='width:200px'>";
     				echo "<option ".($object->value == "" ? "selected" : "")." value=\"\">-</option>";
-            $dwd = "SELECT warngebiet_name,warngebiet_dwd_kennung FROM dwd_warngebiet ORDER BY warngebiet_kreis_stadt_name ASC, warngebiet_dwd_kennung DESC;";
+            $dwd = "SELECT warngebiet_name,warngebiet_dwd_kennung FROM dwd_warngebiet WHERE typ_id != '3' ORDER BY warngebiet_kreis_stadt_name ASC, warngebiet_dwd_kennung DESC;";
             $dwdresult = mysql_query($dwd);
             while ($dwd_regions = mysql_fetch_object($dwdresult)) {
     				  echo "<option ".($object->value == $dwd_regions->warngebiet_dwd_kennung ? "selected" : "")." value=\"".$dwd_regions->warngebiet_dwd_kennung."\">".$dwd_regions->warngebiet_name."</option>";
